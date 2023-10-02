8356d40b963665158198e6ee42e49ffb7c50a448
Vladislav <mypulse@yandex.ru>
2023-10-02 14:24:36 +0300

    22_Task

---
+++ Less3/Task18.py
@@ -0,0 +1,16 @@
+# a = []
+
+# for i in range(10):
+#     a.append(i)
+# print(a)
+
+# a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# for i in range(10):
+#     a.append(i)
+# print(a)
+# print(set(a))
+# print(a[2:7:3])
+
+a = {}
+print(type(a))
